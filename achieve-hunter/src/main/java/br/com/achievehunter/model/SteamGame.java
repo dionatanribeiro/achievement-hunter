e481b5b04e3ef23ae54682de8779e1ffbf08b298
dionatanribeiro <dionatangil@gmail.com>
2015-03-24 00:11:44 -0300

    feat(game-logo-url):
    Criar maneira de construir a url de logo do game

--- achieve-hunter/src/main/java/br/com/achievehunter/model/SteamGame.java
+++ achieve-hunter/src/main/java/br/com/achievehunter/model/SteamGame.java
@@ -2,12 +2,18 @@ package br.com.achievehunter.model;
 
 import java.util.List;
 
-public class SteamGame {
+import org.springframework.beans.factory.InitializingBean;
+
+public class SteamGame implements InitializingBean {
+
+	private static final String GAME_LOGO_URL = "http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/{appId}/{logoHash}.jpg";
 
 	private Integer appId;
 	private String name;
 	private List<SteamAchievement> achievements;
-
+	private String logoHash;
+	private String logoUrl;
+	
 	public SteamGame(Integer appId, String name) {
 		this.appId = appId;
 		this.name = name;
@@ -37,4 +43,25 @@ public class SteamGame {
 		this.achievements = achievements;
 	}
 
+	public String getLogoHash() {
+		return logoHash;
+	}
+
+	public void setLogoHash(String logoHash) {
+		this.logoHash = logoHash;
+	}
+
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.logoUrl = GAME_LOGO_URL.replaceFirst("{appId}", String.valueOf(this.appId)).replaceFirst("{logoHash}", this.logoHash);
+	}
+	
 }
