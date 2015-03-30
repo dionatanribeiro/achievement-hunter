50bd0ff6388d0b43f5f84d9ecb215c993569c4de
dionatanribeiro <dionatangil@gmail.com>
2015-03-30 00:29:17 -0300

    feat(game-stats):
    Recriar logica para leitura da xml do jogo

---
+++ achieve-hunter/src/main/java/br/com/achievehunter/core/steam/GameStats.java
@@ -0,0 +1,90 @@
+package br.com.achievehunter.core.steam;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
+import com.github.koraktor.steamcondenser.steam.community.GameAchievement;
+import com.github.koraktor.steamcondenser.steam.community.SteamGame;
+import com.github.koraktor.steamcondenser.steam.community.SteamId;
+import com.github.koraktor.steamcondenser.steam.community.XMLData;
+
+public class GameStats {
+
+	protected XMLData xmlData;
+	private ArrayList<GameAchievement> achievements;
+	protected SteamGame game;
+
+    protected String hoursPlayed;
+
+    protected String privacyState;
+	
+	public static GameStats createGameStats(Long steamId, Integer appId) throws SteamCondenserException {
+		return new GameStats(steamId, appId);
+	}
+	
+	private GameStats(Long userId, Integer gameId) throws SteamCondenserException{
+		String gameUrl;
+        try {
+        	gameUrl = "appid/" + gameId;
+        	this.xmlData = new XMLData(getBaseUrl(userId, gameUrl) + "?xml=all");
+		} catch (SteamCondenserException e) {
+			SteamId user = SteamId.create(userId);
+			gameUrl = user.getGames().get(gameId).getShortName().trim();
+			this.xmlData = new XMLData(getBaseUrl(userId, gameUrl) + "?xml=1");
+		}
+        
+        this.privacyState = this.xmlData.getString("privacyState");
+        if(this.isPublic()) {
+            Pattern appIdPattern = Pattern.compile("http://steamcommunity\\.com/+app/+([1-9][0-9]*)", Pattern.CASE_INSENSITIVE);
+            Matcher appIdMatcher = appIdPattern.matcher(this.xmlData.getString("game", "gameLink"));
+            appIdMatcher.find();
+            int appId = Integer.parseInt(appIdMatcher.group(1));
+            this.game = SteamGame.create(appId, this.xmlData.getElement("game"));
+            this.hoursPlayed = this.xmlData.getString("stats", "hoursPlayed");
+        }
+        
+	}
+
+	private String getBaseUrl(Long userId, String gameUrl) {
+        return "http://steamcommunity.com/profiles/" + userId + "/stats/" + gameUrl;
+	}
+	
+	/**
+     * Retorna o status dos achievements a partir do usuário e o jogo
+     * 
+     * @return Todos os Achievements pertencentes ao jogo
+     */
+    public ArrayList<GameAchievement> getAchievements() {
+        if(this.achievements == null) {
+            this.achievements = new ArrayList<GameAchievement>();
+            for(XMLData achievementData : this.xmlData.getElements("achievements", "achievement")) {
+                SteamId noUser = null;
+                SteamGame noGame = null;
+            	GameAchievement achievement = new GameAchievement(noUser, noGame, achievementData);
+                this.achievements.add(achievement);
+            }
+        }
+        return achievements;
+    }
+	
+    /**
+     * Returns the game these stats belong to
+     *
+     * @return The game object
+     */
+    public SteamGame getGame() {
+        return this.game;
+    }
+    
+    /**
+     * Returns whether this Steam ID is publicly accessible
+     *
+     * @return <code>true</code> if this Steam ID is publicly accessible
+     */
+    protected boolean isPublic() {
+        return this.privacyState.equals("public");
+    }
+    
+}
---
+++ achieve-hunter/src/test/java/br/com/achievehunter/core/steam/GameStatsTest.java
@@ -0,0 +1,59 @@
+package br.com.achievehunter.core.steam;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
+import com.github.koraktor.steamcondenser.steam.community.GameAchievement;
+
+import br.com.achievehunter.model.steam.Game;
+import br.com.achievehunter.model.steam.Profile;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class GameStatsTest {
+	
+	@Test
+	public void quandoCriaEstatisticasDeUmJogo() throws SteamCondenserException {
+		//Arrange
+		Long steamUserId = 76561198003170021L;
+		Integer darkSoulsId = 211420;
+		
+		//Act
+		GameStats gameStats = GameStats.createGameStats(steamUserId, darkSoulsId);
+		
+		//Assert
+		assertThat("Estatísticas do jogo não podem ser nulas", gameStats, notNullValue());
+	}
+	
+	@Test
+	public void quandoCriaEstatisticasDeAchievementsDoDarkSouls() throws SteamCondenserException {
+		//Arrange
+		Long steamUserId = 76561198003170021L;
+		Integer darkSoulsId = 211420;
+		
+		//Act
+		ArrayList<GameAchievement> achievements = GameStats.createGameStats(steamUserId, darkSoulsId).getAchievements();
+		
+		//Assert
+		assertThat("Lista de achievements não pode ser vazia", achievements.isEmpty(), is(false));
+	}
+	
+	@Test
+	public void quandoCriaEstatisticasDeAchievementsDoFootballManager2012() throws SteamCondenserException {
+		//Arrange
+		Long steamUserId = 76561198003170021L;
+		Integer fm2012Id = 71270;
+		
+		//Act
+		ArrayList<GameAchievement> achievements = GameStats.createGameStats(steamUserId, fm2012Id).getAchievements();
+		
+		//Assert
+		assertThat("Lista de achievements não pode ser vazia", achievements.isEmpty(), is(false));
+	}
+	
+}
