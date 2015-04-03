7ec078d6f42fd73a6bbef07e068e4b71adc78155
dionatanribeiro <dionatangil@gmail.com>
2015-04-03 20:01:48 -0300

    feat(localdatetimeutils):
    Criar conversao de localDateTime para date e localDateTime para String

--- achieve-hunter/src/main/java/br/com/achievehunter/core/utils/LocalDateTimeUtils.java
+++ achieve-hunter/src/main/java/br/com/achievehunter/core/utils/LocalDateTimeUtils.java
@@ -2,12 +2,23 @@ package br.com.achievehunter.core.utils;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public abstract class LocalDateTimeUtils {
 
+	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+	
 	public static LocalDateTime dateToLocalDateTime(Date date) {
 		return date == null ? null : LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+	}
+	
+	public static Date localDateTimeToDate(LocalDateTime localDateTime) {
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+	
+	public static String localDateTimeToString(LocalDateTime localDateTime) {
+		return localDateTime.format(DATE_FORMATTER);
 	}
 	
 }
